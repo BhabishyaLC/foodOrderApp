1236cbfb1298bd90d204c81427dbab063ac5e988
BhabishyaLC <163528869+BhabishyaLC@users.noreply.github.com>
2026-06-02 17:19:00 +0545

    changes

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://fudexpress.netlify.app/"],
+    origin: ["http://localhost:5173","https://fudexpress.netlify.app"],
     credentials: true,
   }),
 );
